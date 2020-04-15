64115ded6eabaea0c1657489b416b9c28c84b24a
Mohammad Hossein Malekpour <43685227+mhmp98@users.noreply.github.com>
2020-04-15 23:33:11 +0430

    JWTmanager added to app

--- directory/apps/__init__.py
+++ directory/apps/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 from directory.apps.config import Development
 
 app = Flask(__name__)
 app.config.from_object(Development)
 
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+jwt = JWTManager(app)
 
 @app.route('/')
 def home():
