3e8b74643485a5aa71c15b6a60aed186a5ee1aa0
Mohammad Hossein Malekpour <43685227+mhmp98@users.noreply.github.com>
2020-04-16 18:06:54 +0430

    move file to directory

---
+++ directory/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+from directory.config import Development
+
+app = Flask(__name__)
+app.config.from_object(Development)
+
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+jwt = JWTManager(app)
+
+@app.route('/')
+def home():
+    return {'message': 'Hello World'}
+
+
+from directory.apps.users_app import users
+from directory.apps.sites_app import sites
+
+app.register_blueprint(users)
+app.register_blueprint(sites)
